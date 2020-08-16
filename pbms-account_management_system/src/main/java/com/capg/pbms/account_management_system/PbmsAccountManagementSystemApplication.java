b3232c70113e6ca9ad51122de9867785b2cfc635
user <user@192.168.0.5>
2020-08-16 22:45:33 +0530

    add

---
+++ pbms-account_management_system/src/main/java/com/capg/pbms/account_management_system/PbmsAccountManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capg.pbms.account_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbmsAccountManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbmsAccountManagementSystemApplication.class, args);
+	}
+
+}
